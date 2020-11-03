6b261811c63c97f0f5d5307069cdc56f686cf9ca
umairbinnawaz <70839368+umairbinnawaz@users.noreply.github.com>
2020-11-03 19:44:55 +0500

    start project

---
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btn1,btn2,btn3,btn4,btn5,btn6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1=findViewById(R.id.btn1);
+        btn2=findViewById(R.id.btn2);
+        btn3=findViewById(R.id.btn3);
+        btn4=findViewById(R.id.btn4);
+        btn5=findViewById(R.id.btn5);
+        btn6=findViewById(R.id.btn6);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn1:
+                break;
+                case R.id.btn2:
+                break;
+                case R.id.btn3:
+                break;
+                case R.id.btn4:
+                break;
+                case R.id.btn5:
+                break;
+                case R.id.btn6:
+                break;
+        }
+
+    }
+}
